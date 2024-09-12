7361d7ac166a03a8bfc766a01fbc17f822a17cd7
Michał Łabuda <mlabuda544@gmail.com>
2024-09-12 17:29:46 +0200

    Change directory name convention

--- src/main/java/com/wszib/userservice/UserServiceApplication.java
+++ src/main/java/com/wszib/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.wszib.user_service;
+package com.wszib.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/wszib/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/wszib/userservice/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wszib.user_service;
+package com.wszib.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
