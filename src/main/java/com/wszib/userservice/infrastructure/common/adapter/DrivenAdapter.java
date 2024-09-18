a68fdb8e53a0ee561b2f20b1511480c6433076ec
Michał Łabuda <mlabuda544@gmail.com>
2024-09-18 23:06:49 +0200

    Add common annotation

---
+++ src/main/java/com/wszib/userservice/infrastructure/common/adapter/DrivenAdapter.java
@@ -0,0 +1,9 @@
+package com.wszib.userservice.infrastructure.common.adapter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface DrivenAdapter {
+}
