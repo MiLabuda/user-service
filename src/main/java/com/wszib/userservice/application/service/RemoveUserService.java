2c2882931ea33d9aa4cb90919ffa68af168138fc
Michał Łabuda <mlabuda544@gmail.com>
2024-10-22 19:26:43 +0200

    extend remove user service with removing from keycloak

--- src/main/java/com/wszib/userservice/application/service/RemoveUserService.java
+++ src/main/java/com/wszib/userservice/application/service/RemoveUserService.java
@@ -1,6 +1,7 @@
 package com.wszib.userservice.application.service;
 
 import com.wszib.userservice.application.ports.in.RemoveUserUseCase;
+import com.wszib.userservice.application.ports.out.KeycloakUserPort;
 import com.wszib.userservice.application.ports.out.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -14,10 +15,12 @@ class RemoveUserService implements RemoveUserUseCase {
     private static final Logger LOGGER = LoggerFactory.getLogger(RemoveUserService.class);
 
     private final UserRepository userRepository;
+    private final KeycloakUserPort keycloakUserPort;
 
     @Override
     public void remove(String id) {
         LOGGER.info("Removing user ...");
         userRepository.delete(id);
+        keycloakUserPort.deleteUser(id);
     }
 }
